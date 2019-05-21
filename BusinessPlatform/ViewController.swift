99d13be30c8603c7ed05ae9644caca4a15285452
Alexey Ivankov <alexey.ivankov@biz-tek.ru>
2019-05-21 14:27:42 +0300

    Initial Commit

---
+++ BusinessPlatform/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusinessPlatform
+//
+//  Created by Alexey Ivankov on 21/05/2019.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
